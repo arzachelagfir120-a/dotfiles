7481f9f38ba0472e833242b0de93d774644aa599
arzachelagfir120-a <arzachelagfir120@gmail.com>
2026-02-13 18:33:32 +0800

    Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,12 +1,9 @@
 if status is-interactive
-    # 1. Jalankan Fastfetch di paling atas
-    # --teatime atau --logo-type bisa ditambah sesuai selera
-    fastfetch --logo-type small
+        fastfetch --logo-type small
 
-    # 2. Hilangkan pesan selamat datang bawaan
     set fish_greeting
 
-    # 3. Inisialisasi Starship (Prompt)
+  
     starship init fish | source
 
     # 4. Aliases (Gabungan dari yang sebelumnya)
